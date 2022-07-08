184b33889cb067ffab80327dce03dcb26da2ec35
Ming <642203604@qq.com>
2022-07-08 18:55:36 +0800

    :art: Simplify API

--- http.go
+++ http.go
@@ -1,16 +1,11 @@
 package bone
 
 import (
-	"context"
-	"encoding/json"
-	"net/http"
+	"github.com/go-kit/kit/transport/http"
 )
 
-var Http = new(httputil)
-
-type httputil struct{}
-
-func (*httputil) EncodeJSON(ctx context.Context, w http.ResponseWriter, rsp interface{}) error {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	return json.NewEncoder(w).Encode(rsp)
-}
+var (
+	NewServer         = http.NewServer
+	EncodeJSONRequest = http.EncodeJSONRequest
+	EncodeXMLRequest  = http.EncodeXMLRequest
+)
--- router.go
+++ router.go
@@ -1,11 +1,6 @@
 package bone
 
 import (
-	"log"
-	"strings"
-
-	"github.com/go-kit/kit/endpoint"
-	"github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
 )
 
@@ -15,11 +10,4 @@ type Router struct {
 
 func NewRouter() *Router {
 	return &Router{Router: mux.NewRouter()}
-}
-
-func (r *Router) RegisterEndpoint(method, path string, e endpoint.Endpoint, dec http.DecodeRequestFunc) *Router {
-	method = strings.ToUpper(method)
-	log.Printf("Registering Endpoint: %s %s\n", method, path)
-	r.Methods(method).Path(path).Handler(http.NewServer(e, dec, Http.EncodeJSON))
-	return r
 }
