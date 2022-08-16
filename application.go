fab3672d3eb6fd0ec616daa1f993ac912ea9149e
Ming <642203604@qq.com>
2022-08-17 01:16:58 +0800

    :sparkles: Add gRPC support

--- application.go
+++ application.go
@@ -3,59 +3,96 @@ package bone
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/facebookgo/inject"
+	"google.golang.org/grpc"
 )
 
 // ApplicationOptions represents the options to create Bone Application
 // instance.
 type ApplicationOptions struct {
-	Host string
-	Port int
+	// Basic part
+	Debug bool
+
+	// HTTP part
+	HttpEnable bool
+	HttpHost   string
+	HttpPort   int
+
+	// gRPC part
+	GrpcEnable bool
+	GrpcHost   string
+	GrpcPort   int
 }
 
 // DefaultApplicationOptions returns the default options to create Bone
-// Application instance.
+// Application instance. Enable HTTP Server and disable gRPC Server by default.
 func DefaultApplicationOptions() *ApplicationOptions {
 	return &ApplicationOptions{
-		Host: "127.0.0.1",
-		Port: 8080,
+		Debug: true,
+
+		HttpEnable: true,
+		HttpHost:   "127.0.0.1",
+		HttpPort:   8080,
+
+		GrpcEnable: false, // Disable gRPC by default
+		GrpcHost:   "127.0.0.1",
+		GrpcPort:   50051,
 	}
 }
 
 // Application defines the struct of Bone Application.
 type Application struct {
-	o *ApplicationOptions
-	g *inject.Graph
-	r *Router
+	o          *ApplicationOptions
+	g          *inject.Graph
+	httpRouter *Router
+	grpcServer *grpc.Server
 }
 
 // NewApplication returns the instance of Bone Application.
 func NewApplication(options *ApplicationOptions) *Application {
 	app := &Application{
-		o: options,
-		g: &inject.Graph{},
-		r: NewRouter(),
+		o:          options,
+		g:          &inject.Graph{},
+		httpRouter: NewRouter(),
+		grpcServer: grpc.NewServer(),
 	}
 
-	app.g.Provide(&inject.Object{Name: "application.router", Value: app.r})
+	if app.o.Debug {
+		log.Println("WARNING: Running in debug mode.")
+	}
+
+	// Inject core components
+	app.Inject(&inject.Object{Name: "application.router", Value: app.httpRouter})
+	app.Inject(&inject.Object{Name: "application.grpc", Value: app.grpcServer})
 
 	return app
 }
 
-func (*Application) register(component Component) {
-	log.Println("Registering Component: " + component.Name())
+// Inject is a low-level API to inject *inject.Object to bone Application.
+// Do NOT use it except must to.
+func (app *Application) Inject(obj *inject.Object) error {
+	return app.g.Provide(obj)
+}
+
+func (app *Application) register(component Component) {
+	if app.o.Debug {
+		log.Println("Registering Component: " + component.Name())
+	}
 	if err := component.Register(); err != nil {
 		panic(err)
 	}
 }
 
-func (*Application) unregister(component Component) {
-	log.Println("Unregistering Component: " + component.Name())
+func (app *Application) unregister(component Component) {
+	if app.o.Debug {
+		log.Println("Unregistering Component: " + component.Name())
+	}
 	component.Unregister()
 }
 
@@ -85,7 +122,9 @@ func (app *Application) Use(components ...Component) {
 
 		<-ch
 		defer os.Exit(0)
-		defer log.Println("Application stopped")
+		if app.o.Debug {
+			defer log.Println("Application stopped")
+		}
 
 		for _, component := range components {
 			defer app.unregister(component)
@@ -95,11 +134,35 @@ func (app *Application) Use(components ...Component) {
 
 // Run starts up the Bone Application.
 func (app *Application) Run() {
-	addr := fmt.Sprintf("%s:%d", app.o.Host, app.o.Port)
-	s := &http.Server{
-		Addr:    addr,
-		Handler: app.r,
+	if app.o.HttpEnable {
+		go func() {
+			addr := fmt.Sprintf("%s:%d", app.o.HttpHost, app.o.HttpPort)
+			s := &http.Server{
+				Addr:    addr,
+				Handler: app.httpRouter,
+			}
+			if app.o.Debug {
+				log.Println("HTTP Server listens on " + addr)
+			}
+			s.ListenAndServe()
+		}()
 	}
-	log.Println("Listen on " + addr)
-	s.ListenAndServe()
+
+	if app.o.GrpcEnable {
+		go func() {
+			addr := fmt.Sprintf("%s:%d", app.o.GrpcHost, app.o.GrpcPort)
+			lis, err := net.Listen("tcp", addr)
+			if err != nil {
+				log.Fatalf("Failed to listen: %v", err)
+			}
+			if app.o.Debug {
+				log.Println("GRPC Server listens on " + addr)
+			}
+			app.grpcServer.Serve(lis)
+		}()
+	}
+
+	select {
+	// Dothing to do, just block here, in order to keep this application on.
+	}
 }
